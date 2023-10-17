e6f9259ef4c3302b0b8ddaa7ed24924f70161a18
David Dickinson <davidrd123@gmail.com>
2023-10-16 18:57:58 -0700

    Changed over to using y-websocket

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Quill from 'quill'
+// import { WebrtcProvider } from 'y-webrtc'
+import { WebsocketProvider } from 'y-websocket'
 import QuillCursors from 'quill-cursors'
 import * as Y from 'yjs'
 import { QuillBinding } from 'y-quill'
@@ -25,8 +27,25 @@ const quill = new Quill(document.querySelector('#editor'), {
   theme: 'snow' // 'bubble' is also great
 });
 
-// Initialize Yjs
-// Initialize Yjs
+// A Yjs document holds the shared document state
 const ydoc = new Y.Doc();
+
+// WebRTC provider for handling the connection
+// const provider = new WebrtcProvider('quill-demo-room', ydoc)
+
+// Websocket provider for handling the connection
+const wsProvider = new WebsocketProvider('ws://localhost:1234', 'quill-demo-room', ydoc)
+
+wsProvider.on('status', event => {
+  console.log(event.status) // logs "connected" or "disconnected"
+})
+
+// Define a shared text type on the document
 const ytext = ydoc.getText('quill');
 const binding = new QuillBinding(ytext, quill);
+
+
+// Accept hot updates
+if (module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('./node_modules/webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
+    index: ['./src/index.js', 'webpack-hot-middleware/client'],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
       template: './src/index.html',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   devtool: 'inline-source-map',
   devServer: {
@@ -23,5 +25,4 @@ module.exports = {
     clean: true,
     publicPath: '/',
   },
-
 };
